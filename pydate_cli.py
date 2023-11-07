416e584dfeb20151d7a1ee3171cb970cea610503
dj9771 <yj2363@nyu.edu>
2023-11-07 18:16:03 -0500

    update merge

--- pydate_cli.py
+++ pydate_cli.py
@@ -14,15 +14,19 @@ def main():
     parser.add_argument("--type", choices=["bakery", "candy", "frozen"])
     parser.add_argument("--price", choices=["low", "medium", "high"])
 
+    parser.add_argument("--cuisine", type=str, default="French")
+    parser.add_argument("--price_range", type=str, default="medium")
+    parser.add_argument("--type", type=str, default="dinner")
+
     args = parser.parse_args()
 
     if args.function == "activity":
         indoor = args.indoor == "True"
         result = activity(indoor=indoor, time=args.time)
-
     elif args.function == "dessert":
         result = dessert(type=args.type, price=args.price)
-
+    elif args.function == "food":
+        result = food(cuisine=args.cuisine, price_range=args.price_range, type=args.type)
 
     print(result)
 
