f0ee0962562ffc171cd54570b7b2a2f027a7683d
dj9771 <yj2363@nyu.edu>
2023-11-07 18:12:32 -0500

    merge with dessert

--- pydate_cli.py
+++ pydate_cli.py
@@ -1,7 +1,7 @@
 import argparse
 from pydate import activity
 from pydate import food
-#from pydate import dessert
+from pydate import dessert
 #from pydate import pickupline
 
 def main():
@@ -10,6 +10,10 @@ def main():
 
     parser.add_argument("--indoor", choices=["True", "False"])
     parser.add_argument("--time", type=str, default="evening")
+
+    parser.add_argument("--type", choices=["bakery", "candy", "frozen"])
+    parser.add_argument("--price", choices=["low", "medium", "high"])
+
     parser.add_argument("--cuisine", type=str, default="French")
     parser.add_argument("--price_range", type=str, default="medium")
     parser.add_argument("--type", type=str, default="dinner")
@@ -19,6 +23,8 @@ def main():
     if args.function == "activity":
         indoor = args.indoor == "True"
         result = activity(indoor=indoor, time=args.time)
+    elif args.function == "dessert":
+        result = dessert(category=args.type, price=args.price)
     elif args.function == "food":
         result = food(cuisine=args.cuisine, price_range=args.price_range, type=args.type)
     
