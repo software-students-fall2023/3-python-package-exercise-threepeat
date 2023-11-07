2edd95501233151a83d7d0b597fbf9d459a8aead
Gabriel <gmp9469@nyu.edu>
2023-11-06 22:23:37 -0500

    changing return format

--- pydate/activities.py
+++ pydate/activities.py
@@ -17,6 +17,7 @@ def activity(indoor=True, time="evening"):
 
     if filtered_activities:
         recommended_activity = random.choice(filtered_activities)
+        recommended_activity = f"Activity: {recommended_activity['activity']}\nindoor: {'yes' if recommended_activity['indoor'] else 'no'}\ntime: {recommended_activity['time']}"
     else:
         recommended_activity = "Stay home and relax."
     return recommended_activity
