744a6f2104147f946389086829c3f1b2a3cd07a3
Gabriel <gmp9469@nyu.edu>
2023-11-08 02:18:08 -0500

    hardcode json objects

--- pydate/dessert.py
+++ pydate/dessert.py
@@ -3,10 +3,115 @@ import random
 import os
 import pkg_resources
 
-def dessert(type, price):
-  json_path = pkg_resources.resource_filename(__name__, 'data/desserts.json')
+shops = [
+    {
+      "name": "Culture: An American Yogurt Co",
+      "price": "medium",
+      "type": "frozen"
+    },
+    {
+      "name": "Ralphs Famous Italian Ices & Ice Cream",
+      "price": "low",
+      "type": "frozen"
+    },
+    {
+      "name": "Chinatown Ice Cream Factory",
+      "price": "low",
+      "type": "frozen"
+    },
+    {
+      "name": "Van Leeuwen",
+      "price": "medium",
+      "type": "frozen"
+    },
+    {
+      "name": "Venchi",
+      "price": "high",
+      "type": "frozen"
+    },
+    {
+      "name": "Amorino",
+      "price": "high",
+      "type": "frozen"
+    },
+
+    {
+      "name": "Insomnia Cookies",
+      "price": "low",
+      "type": "bakery"
+    },
+    {
+      "name": "Paris Baguette",
+      "price": "medium",
+      "type": "bakery"
+    },
+    {
+      "name": "Breads Bakery",
+      "price": "medium",
+      "type": "bakery"
+    },
+    {
+      "name": "Levain Bakery",
+      "price": "medium",
+      "type": "bakery"
+    },
+    {
+      "name": "Dominique Ansel Bakery",
+      "price": "high",
+      "type": "bakery"
+    },
+    {
+      "name": "Lady M Confections",
+      "price": "high",
+      "type": "bakery"
+    },
+    
+    {
+      "name": "See's Candies",
+      "price": "low",
+      "type": "candy"
+    },
+    {
+      "name": "Economy Candy",
+      "price": "low",
+      "type": "candy"
+    },
+    {
+      "name": "M&M's New York",
+      "price": "low",
+      "type": "candy"
+    },
+    {
+      "name": "Dylan's Candy Bar",
+      "price": "medium",
+      "type": "candy"
+    },
+    {
+      "name": "Lindt Chocolate Shop",
+      "price": "medium",
+      "type": "candy"
+    },
+    {
+      "name": "BonBon - A Swedish Candy Co",
+      "price": "medium",
+      "type": "candy"
+    },
+    {
+      "name": "Sugarfina",
+      "price": "high",
+      "type": "candy"
+    },
+    {
+      "name": "ROYCE' Chocolate",
+      "price": "high",
+      "type": "candy"
+    }
+]
+
+def dessert(type="candy", price="low"):
+  """json_path = pkg_resources.resource_filename(__name__, 'data/desserts.json')
   with open(json_path, "r") as file:
-    shops = json.load(file)
+    shops = json.load(file)"""
 
 
   found_shops = []
--- pydate/food.py
+++ pydate/food.py
@@ -4,6 +4,81 @@ import os
 import six
 import pkg_resources
 
+foods = [
+    {
+        "name": "Foie gras",
+        "cuisine": "French",
+        "price_range": "high",
+        "meal": "dinner"
+    },
+    {
+        "name": "Chicken and Mushroom Fricassee",
+        "cuisine": "French",
+        "price_range": "low",
+        "meal": "dinner"
+    },
+    {
+        "name": "Marseille-Style Shrimp Stew",
+        "cuisine": "French",
+        "price_range": "medium",
+        "meal": "dinner"
+    },
+    {
+        "name": "Short Rib Bourguignon",
+        "cuisine": "French",
+        "price_range": "medium",
+        "meal": "dinner"
+    },
+    {
+        "name": "Lobster Thermidor",
+        "cuisine": "French",
+        "price_range": "medium",
+        "meal": "dinner"
+    },
+    {
+        "name": "Dumplings",
+        "cuisine": "Chinese",
+        "price_range": "low",
+        "meal": "lunch"
+    },
+    {
+        "name": "Boiled Fish With Pickled Mustard",
+        "cuisine": "Chinese",
+        "price_range": "high",
+        "meal": "lunch"
+    }, 
+    {
+        "name": "Stinky Mandarin Fish",
+        "cuisine": "Chinese",
+        "price_range": "high",
+        "meal": "lunch"
+    }, 
+    {
+        "name": "Chinese Savior Crepe",
+        "cuisine": "Chinese",
+        "price_range": "low",
+        "meal": "breakfast"
+    }, 
+    {
+        "name": "Deep-fried Dough Sticks",
+        "cuisine": "Chinese",
+        "price_range": "low",
+        "meal": "breakfast"
+    }, 
+    {
+        "name": "Noodles with Soy Bean Paste",
+        "cuisine": "Chinese",
+        "price_range": "low",
+        "meal": "lunch"
+    },
+    {
+        "name": "Sautéed Bullfrog in Chili Sauce",
+        "cuisine": "Chinese",
+        "price_range": "high",
+        "meal": "dinner"
+    }
+]
+
 def food(cuisine="French", price_range="medium", meal="dinner"):
     if(not isinstance(cuisine, six.string_types) or not isinstance(price_range, six.string_types) or not isinstance(meal, six.string_types)):
         return "Your filter is invalid. Try again."
